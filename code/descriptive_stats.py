3cbe620ed55c392fa104b7561f34ddb70e8724c3
Michael Hanke <michael.hanke@gmail.com>
2016-08-25 09:39:25 +0200

    Report basic statistics

--- code/descriptive_stats.py
+++ code/descriptive_stats.py
@@ -2,6 +2,7 @@
 
 from os.path import join as opj
 import numpy as np
+import itertools
 
 
 def timestamp2sec(ts, fps=25):
@@ -51,6 +52,45 @@ def _ft(key, value, fmt='s'):
     return tex.format(key=key, value=value)
 
 
+def _get_n_reoccurrences(vals):
+    return [len(list(i[1])) - 1  # subtract one for the first occurrence
+            # loop over each unique value
+            for i in itertools.groupby(
+                # sort for next grouping
+                sorted([i[0]
+                        # get blocks of consecutive identical values
+                        for i in itertools.groupby(vals)]))]
+
+
+def _get_counts(vals):
+    return [len(list(v))
+            for k, v in itertools.groupby(sorted(vals))]
+
+
+def _get_counts_consecutive(vals):
+    return [len(list(v))
+            for k, v in itertools.groupby(vals)]
+
+
+def _print_location_info(vals, label):
+    locations = np.unique(vals)
+    print('% QA unique: {}'.format(repr(list(locations))))
+    print(_ft('N{}s'.format(label), len(locations), 'd'))
+    # revisited locations
+    # shots per location
+    # consecutive shots per location
+    for fx, proplabel in (
+            (_get_n_reoccurrences, 'NTimes{}sRevisited'),
+            (_get_counts, 'ShotsPer{}'),
+            (_get_counts_consecutive, 'ConsecShotsPer{}')):
+        freq = fx(vals)
+        proplabel = proplabel.format(label)
+        print(_ft('{}Mean'.format(proplabel), np.mean(freq), '.1f'))
+        print(_ft('{}Min'.format(proplabel), min(freq), '.0f'))
+        print(_ft('{}Max'.format(proplabel), max(freq), '.0f'))
+        print(_ft('{}Median'.format(proplabel), np.median(freq), '.0f'))
+
+
 def print_descriptive_stats_as_tex(data):
     print(_ft('NShots', len(data), 'd'))
     shot_durations = np.diff(data['time'])
@@ -58,6 +98,28 @@ def print_descriptive_stats_as_tex(data):
     print(_ft('ShotLengthMin', shot_durations.min(), '.2f'))
     print(_ft('ShotLengthMax', shot_durations.max(), '.2f'))
     print(_ft('ShotLengthSD', shot_durations.std(), '.2f'))
+    # without paramount and final black frame
+    _print_location_info(shots[1:-1]['major_location'], 'MajorLocation')
+    _print_location_info(shots[1:-1]['setting'], 'Setting')
+    _print_location_info(shots[1:-1]['locale'], 'Locale')
+
+    # interior/exterior
+    print('% QA unique: {}'.format(np.unique(shots[1:-1]['int_or_ext'])))
+    print(_ft('NExteriorShots', sum(shots[1:-1]['int_or_ext'] == b'ext'), 'd'))
+    print(_ft('NInteriorShots', sum(shots[1:-1]['int_or_ext'] == b'int'), 'd'))
+
+    # time of day
+    print('% QA unique: {}'.format(np.unique(shots[1:-1]['time_of_day'])))
+    print(_ft('NDayShots', sum(shots[1:-1]['time_of_day'] == b'day'), 'd'))
+    print(_ft('NNightShots', sum(shots[1:-1]['time_of_day'] == b'night'), 'd'))
+
+    # time
+    print('% QA unique: {}'.format(np.unique(shots[1:-1]['flow_of_time'])))
+    for c, label in ((b'0', 'NoJump'), (b'+', 'SmallJump'),
+                     (b'++', 'LargeJump'), (b'-', 'Flashback')):
+        print(_ft('NShotsTime{}'.format(label),
+                  sum(shots[1:-1]['flow_of_time'] == c),
+                  'd'))
 
 
 if __name__ == '__main__':
